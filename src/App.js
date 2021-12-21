07b8e5384d9c89a4688b018aec7b2c6f413f1ef5
Sebastian Naicker <sebastian.naicker@johnlewis.co.uk>
2021-12-21 15:10:20 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [shells, setShells] = useState([false,false,false,false,true])
+  const [played, setPlayed] = useState(false)
+  const [message, setMessage] = useState(null)
+  
+  const randomiseShells = () => {
+    const newShells = [...shells]
+    setShells(newShells.sort(() => Math.random() > .5 ? 1 : -1))
+    setMessage(null)
+    setPlayed(false)
+  }
+  
+  const validateWin = (shell) => () => {
+    setPlayed(true)
+    
+    if (shell) {
+      setMessage('Correct!')
+    } else {
+      setMessage('Sorry, wrong guess!')
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Game of Shells</h1>
+      
+      {message ? (
+        <h2>{message}</h2>
+      ): (
+        <h2>Find the shell. Hint: {`[${shells.toString()}]`}</h2>
+      )}
+      
+      <div className='shells'>
+        {shells.map((shell, i) => (
+          <button className='shell' onClick={validateWin(shell)} key={i.toString()} disabled={played} />
+        ))}
+      </div>
+      <div className='new-game-container'>
+        <button onClick={randomiseShells}>New game</button>
+      </div>
     </div>
   );
 }
